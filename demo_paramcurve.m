1d0c53a8803dc4f5b275e42e1c46a90e123b5723
ahbarnett <ahb@math.dartmouth.edu>
2020-09-08 18:52:29 -0400

    readme pic ugh

--- demo_paramcurve.m
+++ demo_paramcurve.m
@@ -12,7 +12,7 @@ bxp = perispecdiff(bx); byp = perispecdiff(by);  % derivatives wrt t (if smooth)
 [xq yq wq] = curveareaquad(bx,by,bxp,byp,m);   % areal quadrature
 % The rest of this code is same as demo_radial.m ...
 tol = 1e-9;            % desired accuracy
-verb = 1;              % verbosity
+verb = 2;              % verbosity
 
 ximax = 1.5; ngrid = 1e3;   % million-pt grid
 [u xigrid] = fresnap_grid(xq, yq, wq, lambdaz, ximax, ngrid, tol, verb);
@@ -32,7 +32,7 @@ xlabel('\xi'); ylabel('\eta'); title('log_{10} |u|^2, occulter, grid');
 if verb>1, figure(2); clf; imagesc(xigrid,xigrid,abs(u)'.^2); % fig for repo
   colormap(hot(256)); colorbar; hold on; plot(bx,by,'w-'); axis xy equal tight;
   xlabel('\xi'); ylabel('\eta'); title('|u|^2, occulter, grid');
-  v=caxis; caxis([0 v(2)]); drawnow; print -dpng -r300 kite_grid.png
+  v=caxis; caxis([0 v(2)]); drawnow; print -dpng -r50 kite_grid.png
 end
 
 M=1e6; xi = ximax*(2*rand(M,1)-1); eta = ximax*(2*rand(M,1)-1);  % million pts
